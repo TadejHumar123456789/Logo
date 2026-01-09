ec73991e4dcda22ac84356006b8f9e710f6b37dd
TadejHumar123456789 <167509732+TadejHumar123456789@users.noreply.github.com>
2026-01-09 18:44:54 +0100

    Delete Js directory

--- Js/barva.js
+++
@@ -1,7 +0,0 @@
-const colorPicker = document.getElementById("colorPicker");
-const logo = document.getElementById("logo");
-
-// Dogodek ob spremembi barve
-colorPicker.addEventListener("input", () => {
-    logo.setAttribute("fill", colorPicker.value);
-});
